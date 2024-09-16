2408bf90e1a198f1439a0cd3f057374a01cfc976
Pushparaj M <pushparaj-fullstack-dev@outlook.com>
2024-09-16 22:48:00 +0530

    am charts integration

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -42,3 +42,95 @@ function copyToClipboard() {
 
     alert("Phone number copied to clipboard: " + phoneNumber);
 }
+
+am5.ready(function () {
+
+    var root_1 = am5.Root.new("chartdiv_1");
+    var root_2 = am5.Root.new("chartdiv_2");
+    var root_3 = am5.Root.new("chartdiv_3");
+
+    root_1.setThemes([
+        am5themes_Animated.new(root_1)
+    ]);
+    root_2.setThemes([
+        am5themes_Animated.new(root_2)
+    ]);
+    root_3.setThemes([
+        am5themes_Animated.new(root_3)
+    ]);
+
+    var chart_1 = root_1.container.children.push(am5percent.PieChart.new(root_1, {
+        layout: root_1.verticalLayout
+    }));
+    var chart_2 = root_2.container.children.push(am5percent.PieChart.new(root_2, {
+        layout: root_2.verticalLayout
+    }));
+    var chart_3 = root_3.container.children.push(am5percent.PieChart.new(root_3, {
+        layout: root_3.verticalLayout
+    }));
+
+    var series_1 = chart_1.series.push(am5percent.PieSeries.new(root_1, {
+        valueField: "value",
+        categoryField: "category"
+    }));
+    var series_2 = chart_2.series.push(am5percent.PieSeries.new(root_2, {
+        valueField: "value",
+        categoryField: "category"
+    }));
+    var series_3 = chart_3.series.push(am5percent.PieSeries.new(root_3, {
+        valueField: "value",
+        categoryField: "category"
+    }));
+
+    series_1.data.setAll([
+        { value: 7.96, category: "Sem III" },
+        { value: 8.14, category: "Sem IV" },
+        { value: 7.55, category: "Sem V" },
+        { value: 7.26, category: "Sem VI" },
+        { value: 8.10, category: "Sem VII" },
+        { value: 8.00, category: "Sem VIII" },
+    ]);
+    series_2.data.setAll([
+        { value: 80.33, category: "Sem I" },
+        { value: 76.88, category: "Sem II" },
+        { value: 87.57, category: "Sem III" },
+        { value: 83.28, category: "Sem IV" },
+        { value: 88.75, category: "Sem V" },
+        { value: 97.57, category: "Sem VI" },
+    ]);
+    series_3.data.setAll([
+        { value: 93, category: "Tamil" },
+        { value: 68, category: "English" },
+        { value: 76, category: "Mathematics" },
+        { value: 78, category: "Science" },
+        { value: 95, category: "Social Science" },
+    ]);
+
+    var legend_1 = chart_1.children.push(am5.Legend.new(root_1, {
+        centerX: am5.percent(50),
+        x: am5.percent(50),
+        marginTop: 15,
+        marginBottom: 15
+    }));
+    var legend_2 = chart_2.children.push(am5.Legend.new(root_2, {
+        centerX: am5.percent(50),
+        x: am5.percent(50),
+        marginTop: 15,
+        marginBottom: 15
+    }));
+    var legend_3 = chart_3.children.push(am5.Legend.new(root_3, {
+        centerX: am5.percent(50),
+        x: am5.percent(50),
+        marginTop: 15,
+        marginBottom: 15
+    }));
+
+
+    legend_1.data.setAll(series_1.dataItems);
+    legend_2.data.setAll(series_2.dataItems);
+    legend_3.data.setAll(series_3.dataItems);
+
+    series_1.appear(1000, 100);
+    series_2.appear(1000, 100);
+    series_3.appear(1000, 100);
+});
